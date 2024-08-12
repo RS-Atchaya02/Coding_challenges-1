aac1d67805fdd33f386f5136dfedba04caa06eff
Atchaya RS <142617187+RS-Atchaya02@users.noreply.github.com>
2024-08-12 22:48:20 +0530

    even.js
    Find the Even number in an given array

---
+++ even.js
@@ -0,0 +1,13 @@
+function even(arr) {
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) 
+    {
+        if (i % 2 === 0) {
+            count += 1;
+        }
+    }
+    return count;
+}
+
+let arr = [56,89,4,5,22,10];
+console.log(even(arr));
